d99aa8dd4a120c248eaaf97dca59f14842a26988
Sathish <sathish@alethea.in>
2023-07-10 15:17:12 +0530

    basic components. structure

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import VerticalTabs from './pages/Home';
+
+function App() {
+
+  return (
+    <>
+      <VerticalTabs />
+    </>
+  );
+}
+
+export default App;
