0e0cf2e841f342eb2b45a738c71e7b7e6e333338
Sathish <sathish@alethea.in>
2023-07-10 18:56:05 +0530

    Handled CSs

--- src/utilities/Routes/Layout.tsx
+++ src/utilities/Routes/Layout.tsx
@@ -1,19 +1,28 @@
-import { Link } from "react-router-dom";
-import RouterConfig from "./Routes";
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import RouterConfig from './Routes';
+import { List, ListItem, ListItemText } from '@mui/material';
+
 
 const Layout = () => {
+    const location = useLocation();
+
     return (
         <>
-            <nav>
-                <ul>
-                    {RouterConfig.map((route, i) => {
-                        return (
-                            <li key={i}>
-                                <Link to={route.path}>{route.sidebarName}</Link>
-                            </li>
-                        );
-                    })}
-                </ul>
+            <nav className='navBar'>
+                <List component="ul">
+                    {RouterConfig.map((route, i) => (
+                        <ListItem
+                            key={i}
+                            button
+                            component={Link}
+                            to={route.path}
+                            className={location.pathname === route.path ? 'activeLink' : ''}
+                        >
+                            <ListItemText primary={route.sidebarName} />
+                        </ListItem>
+                    ))}
+                </List>
             </nav>
         </>
     );
